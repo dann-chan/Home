b385b6e891a79ee0c0ee6961eb7c44f199d53434
Dani <78885644+dann-chan@users.noreply.github.com>
2021-02-12 16:01:28 -0500

    Initial commit

---
+++ precache-manifest.1f67d233420eae6b33d241e661b8ead3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86002141c45771fa3336a8623c1dab0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a370618fc86c2509ab55",
+    "url": "/static/css/main.a1249a53.chunk.css"
+  },
+  {
+    "revision": "7f8d03e49bb00274b05c",
+    "url": "/static/js/2.22afbfcc.chunk.js"
+  },
+  {
+    "revision": "772341699553daad6bcd7d8925a6b28f",
+    "url": "/static/js/2.22afbfcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a370618fc86c2509ab55",
+    "url": "/static/js/main.34d8bb8d.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  }
+]);
